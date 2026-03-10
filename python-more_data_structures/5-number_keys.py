481381c5d585cd7f9d58f0ca71d35d36d2877646
Morel121 <n.morel@alustudent.com>
2026-03-10 03:54:20 -0700

    five

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+    
+    Args:
+        a_dictionary: The dictionary to evaluate.
+        
+    Returns:
+        The total count of keys as an integer.
+    """
+    # The len() function returns the number of items (keys) in a dictionary
+    return len(a_dictionary)
